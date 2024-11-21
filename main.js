36b88acf2ea459e00030fc81d5ef609b56d0fe99
Alejandro Gomez Mateo <alexjtk40@gmail.com>
2024-11-21 15:44:40 -0400

    Commit inicial

---
+++ main.js
@@ -0,0 +1,101 @@
+
+
+//Mostar el modal de imagenes cuando hago click en la imagen principal.
+
+const imagesModal = document.querySelector('.modal-gallery_background');
+const closeModalBtn = document.querySelector('.modal-gallery_close');
+
+imageContainer.addEventListener('click', ()=>{
+    imagesModal.style.display = 'grid';
+});
+
+closeModalBtn.addEventListener('click', ()=>{
+    imagesModal.style.display = 'none';
+});
+
+// Cambiar las imagenes principales desde los thumnails
+
+let thumbnails = document.querySelectorAll('.gallery_thumnail');
+thumbnails = [...thumbnails]
+
+thumbnails.forEach(thumbnail =>{
+    thumbnail.addEventListener('click', event=>{
+        console.log(event.target.id);
+        imageContainer.style.backgroundImage = `url('../images/image-product-${event.target.id}.jpg')`;
+    })
+});
+
+// Cambiar la imagenes principales desde los thumbnails en el Modal.
+
+let modalthumbnails = document.querySelectorAll('.modal-gallery_thumnail');
+const modalImageContainer = document.querySelector('.modal-gallery_image-container');
+modalthumbnails = [...modalthumbnails]
+
+modalthumbnails.forEach(modalthumbnails =>{
+    modalthumbnails.addEventListener('click', event=>{
+        console.log(event.target.id.slice(-1));
+        modalImageContainer.style.backgroundImage = `url('../images/image-product-${event.target.id.slice(-1)}.jpg')`;
+    });
+});
+
+// Cambiar la imagenes principal de modal desde flechas.
+
+const previusModalBtn = document.querySelector('.modal-gallery_previus');
+const nextModalBtn = document.querySelector('.modal-gallery_next');
+
+nextModalBtn.addEventListener('click', ()=>{
+    changeNextImage(modalImageContainer);
+});
+
+previusModalBtn.addEventListener('click', ()=>{
+    changePreviusImage(modalImageContainer);
+});
+
+// Mostrar el navbalMovile cuando presiono el menu de hamburguesa.
+
+let showMenu = document.querySelector('.modal_navbar_items')
+
+showMenu.addEventListener('click', ()=>{
+   showMenu.style.display = 'block'; 
+});
+
+
+//Funciones
+
+ function drawProductInModal(){
+    productContainer.innerHTML = `
+    <div class="cart-modal_details-container">
+        <img class="cart-modal_image" src="./images/image-product-1-thumbnail.jpg" alt="">
+        <div>
+            <p class="cart-modal_product">Autum Limited Edition...</p>
+            <p class="cart-modal_price">$125 x3 <span>$375.00</span></p>
+        </div>
+        <img class="cart-modal_delete" src="./images/icon-delete.svg" alt="delete">
+    </div>
+        <button class="cart-modal_checkout">Checkout</button>`; 
+    deleteProduct();
+    let priceModal = document.querySelector('.cart-modal_price');
+    priceModal.innerHTML = `$125 x${lastValue} <span>$${lastValue*125}</span>`;
+}
+ 
+
+function changeNextImage(imgContainer){
+    if(imgIndex == 4){
+        imgIndex = 1;
+    }else{
+        imgIndex++;
+    }
+
+    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`;
+}
+
+function changePreviusImage(imgContainer){
+    if(imgIndex == 1){
+        imgIndex = 4;
+    }else{
+        imgIndex--;
+    }
+
+    imgContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`;
+
+}
